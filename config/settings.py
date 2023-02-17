fea3d166c7ebb0d40e7c36aee3443787d08863ac
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-02-17 12:03:29 +0000

    added staticfiles config

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = BASE_DIR / "src/static"
+STATIC_ROOT = BASE_DIR / "src/staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
