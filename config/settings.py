0050e75c76c35314056f2636bfedcd0656a11897
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-02-17 12:36:18 +0000

    added behave_django

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "behave_django",
     # local
     "src.accounts",
 ]
