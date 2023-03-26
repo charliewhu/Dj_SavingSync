c191a4cc6019e869ad7698822d6c6c98c5edb1aa
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-26 18:19:26 +0100

    added placeholder e2e test

---
+++ tests/features/steps/definitions.py
@@ -0,0 +1,16 @@
+from behave import given, when, then
+
+
+@given("this step exists")
+def step_impl(context):
+    pass
+
+
+@when("I run {string}")
+def step_when(context, string):
+    pass
+
+
+@then("I should see the behave tests run")
+def step_then(context):
+    pass
