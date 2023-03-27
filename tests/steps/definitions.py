c8563e34e7c8e288182e7480c5d8e5b43560f9a4
Charlie <charlieavery123@hotmail.co.uk>
2023-03-27 21:12:52 +0100

    added example playwright navigation

--- tests/steps/definitions.py
+++ tests/steps/definitions.py
@@ -1,9 +1,12 @@
 from behave import given, when, then
+from playwright.sync_api import expect
 
 
 @given("this step exists")
 def step_impl(context):
-    pass
+    # assert Playwright navigates to context url
+    context.page.goto(context.base_url)
+    context.test.assertIn(context.base_url, context.page.url)
 
 
 @when("I run {string}")
