78cc47d0dd21e6a45b47c0a0653496c9153b21dd
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-11 20:11:27 +0100

    corrected number to string

--- tests/features/test_add_salary.py
+++ tests/features/test_add_salary.py
@@ -16,7 +16,7 @@ class TestAddSalary(BasePlaywrightTestCase):
         self.page.locator('select[name="cashflow"]').select_option(label="Income")
         self.page.locator('select[name="type"]').select_option(label="Salary")
         self.page.get_by_placeholder("Name").fill("Salary")
-        self.page.get_by_placeholder("Amount").fill(100)
+        self.page.get_by_placeholder("Amount").fill("100")
         self.page.get_by_role("button", name="add").click()
 
         assert self.page.get_by_placeholder("Name").inner_text() == ""
@@ -26,7 +26,7 @@ class TestAddSalary(BasePlaywrightTestCase):
         self.page.locator('select[name="cashflow"]').select_option(label="Expense")
         self.page.locator('select[name="type"]').select_option(label="Regular Bill")
         self.page.get_by_placeholder("Name").fill("Phone")
-        self.page.get_by_placeholder("Amount").fill(50)
+        self.page.get_by_placeholder("Amount").fill("50")
         self.page.get_by_role("button", name="add").click()
 
         assert self.page.get_by_placeholder("Name").inner_text() == ""
