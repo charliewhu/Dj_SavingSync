caf94b4cf8d98dbc33495b6c60abf0ed32129a3e
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-12 18:49:02 +0100

    added create view

--- src/cashflows/views.py
+++ src/cashflows/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 
 # Create your views here.
 def home(request: HttpRequest):
     return render(request, "home.html")
+
+
+def create(request: HttpRequest):
+    return redirect(reverse("home"))
