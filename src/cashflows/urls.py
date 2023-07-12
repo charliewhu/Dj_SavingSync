c5d198e445f1bebb44555b63828e264dbc3db4d8
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-12 18:48:53 +0100

    added create url

--- src/cashflows/urls.py
+++ src/cashflows/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import create, home
 
 urlpatterns = [
-    path("", home),
+    path("", home, name="home"),
+    path("create", create, name="create"),
 ]
