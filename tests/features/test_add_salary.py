f5183fbab02c9d33b268442e99eaf36eb5291da5
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-12 19:31:57 +0100

    changed locators

--- tests/features/test_add_salary.py
+++ tests/features/test_add_salary.py
@@ -13,22 +13,22 @@ class TestAddSalary(BasePlaywrightTestCase):
 
         self.page.goto(f"{self.live_server_url}/")
 
-        self.page.locator('select[name="cashflow"]').select_option(label="Income")
-        self.page.locator('select[name="type"]').select_option(label="Salary")
-        self.page.get_by_placeholder("Name").fill("Salary")
-        self.page.get_by_placeholder("Amount").fill("100")
+        self.page.locator('select[name="type"]').select_option(label="Income")
+        self.page.locator('select[name="source"]').select_option(label="Salary")
+        self.page.locator('input[name="name"]').fill("Salary")
+        self.page.locator('input[name="amount"]').fill("100")
         self.page.get_by_role("button", name="add").click()
 
-        assert self.page.get_by_placeholder("Name").inner_text() == ""
+        assert self.page.locator('input[name="name"]').inner_text() == ""
         assert self.page.get_by_test_id("monthly-balance").inner_text() == "100"
         assert self.page.get_by_test_id("income-list-item").count() == 1
 
-        self.page.locator('select[name="cashflow"]').select_option(label="Expense")
-        self.page.locator('select[name="type"]').select_option(label="Regular Bill")
-        self.page.get_by_placeholder("Name").fill("Phone")
-        self.page.get_by_placeholder("Amount").fill("50")
+        self.page.locator('select[name="type"]').select_option(label="Expense")
+        self.page.locator('select[name="source"]').select_option(label="Regular Bill")
+        self.page.locator('input[name="name"]').fill("Phone")
+        self.page.locator('input[name="amount"]').fill("50")
         self.page.get_by_role("button", name="add").click()
 
-        assert self.page.get_by_placeholder("Name").inner_text() == ""
+        assert self.page.locator('input[name="name"]').inner_text() == ""
         assert self.page.get_by_test_id("monthly-balance").inner_text() == "50"
         assert self.page.get_by_test_id("expense-list-item").count() == 1
