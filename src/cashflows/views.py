2f22d3540a7b1ef39ffe4ed5d40ba09a7b0ff396
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-12 19:20:12 +0100

    pass form to template

--- src/cashflows/views.py
+++ src/cashflows/views.py
@@ -2,10 +2,21 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from src.cashflows.forms import CashflowForm
+
 
 # Create your views here.
 def home(request: HttpRequest):
-    return render(request, "home.html")
+    form = CashflowForm()
+    context = {
+        "form": form,
+    }
+
+    return render(
+        request,
+        "home.html",
+        context,
+    )
 
 
 def create(request: HttpRequest):
