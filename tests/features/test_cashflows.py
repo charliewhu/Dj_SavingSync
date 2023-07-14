26615d11be6f2a772a1165ad93710c8896f9b851
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-14 20:48:03 +0100

    wrote delete test

--- tests/features/test_cashflows.py
+++ tests/features/test_cashflows.py
@@ -1,8 +1,15 @@
 from ..base import BasePlaywrightTestCase
 
+from model_bakery import baker
+from src.cashflows.models import Cashflow
 
-class TestAddSalary(BasePlaywrightTestCase):
-    def test_add_salary(self):
+
+class TestCashflows(BasePlaywrightTestCase):
+    def setUp(self):
+        self.page.goto(f"{self.live_server_url}/")
+        self.income_list_item_id = "income-list-item"
+
+    def test_create(self):
         """
         Given I am on the home page
         When I add an income of "100"
@@ -11,19 +18,19 @@ class TestAddSalary(BasePlaywrightTestCase):
         Then I should see "50" in the monthly budget remaining section
         """
 
-        self.page.goto(f"{self.live_server_url}/")
-
         self.page.locator('select[name="type"]').select_option(label="Income")
         self.page.locator('select[name="source"]').select_option(label="Salary")
         self.page.locator('input[name="name"]').fill("Salary")
         self.page.locator('input[name="amount"]').fill("100")
         self.page.get_by_role("button", name="add").click()
 
+        cashflow_item_count = self.page.get_by_test_id(self.income_list_item_id).count
+
         assert self.page.locator('input[name="name"]').inner_text() == ""
         assert "100" in self.page.get_by_test_id("monthly-balance").inner_text()
-        assert self.page.get_by_test_id("income-list-item").count() == 1
+        assert cashflow_item_count() == 1
 
-        self.page.locator('select[name="type"]').select_option(label="Expense")
+        self.page.locator('select[name="type"]').select_option(label="Income")
         self.page.locator('select[name="source"]').select_option(label="Regular Bill")
         self.page.locator('input[name="name"]').fill("Phone")
         self.page.locator('input[name="amount"]').fill("50")
@@ -32,3 +39,21 @@ class TestAddSalary(BasePlaywrightTestCase):
         assert self.page.locator('input[name="name"]').inner_text() == ""
         assert "50" in self.page.get_by_test_id("monthly-balance").inner_text()
         assert self.page.get_by_test_id("expense-list-item").count() == 1
+
+    def test_delete(self):
+        """
+        Given I am on the home page
+        And I have 1 existing cashflow
+        When I delete the cashflow
+        Then I should not see any cashflows
+        """
+
+        self.cashflow = baker.make(Cashflow)
+
+        cashflow_item_count = self.page.locator(self.income_list_item_id).count
+
+        assert cashflow_item_count() == 1
+
+        self.page.locator('button[name="Delete"]').click()
+
+        assert cashflow_item_count() == 0
