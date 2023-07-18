cc44defe765938040b82ddcb77e94b8825561057
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-18 20:34:46 +0100

    added min value on model

--- src/cashflows/models.py
+++ src/cashflows/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 from .querysets import CashflowQuerySet
 
@@ -18,6 +19,8 @@ class Cashflow(models.Model):
     type = models.CharField(max_length=10, choices=TYPE_CHOICES)
     source = models.CharField(max_length=20, choices=SOURCE_CHOICES)
     name = models.CharField(max_length=30)
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    amount = models.DecimalField(
+        max_digits=9, decimal_places=2, validators=[MinValueValidator(0)]
+    )
 
     objects = CashflowQuerySet.as_manager()
