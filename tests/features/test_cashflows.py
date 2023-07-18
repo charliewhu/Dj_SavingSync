7f2aa10119676cee4be8288eb22f2313f0330c7a
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-18 19:50:47 +0100

    refactored test for robustness

--- tests/features/test_cashflows.py
+++ tests/features/test_cashflows.py
@@ -22,29 +22,45 @@ class TestCashflows(BasePlaywrightTestCase):
         Then I should see "50" in the monthly budget remaining section
         """
 
+        income_item = {
+            "name": "Salary",
+            "amount": "100",
+        }
+
+        expense_item = {
+            "name": "Phone",
+            "amount": "50",
+        }
+
         self.page.locator('select[name="type"]').select_option(label="Income")
         self.page.locator('select[name="source"]').select_option(label="Salary")
-        self.page.locator('input[name="name"]').fill("Salary")
-        self.page.locator('input[name="amount"]').fill("100")
+        self.page.locator('input[name="name"]').fill(income_item["name"])
+        self.page.locator('input[name="amount"]').fill(income_item["amount"])
         self.page.get_by_role("button", name="add").click()
 
-        # assert self.page.locator('input[name="name"]').inner_text() == ""
-        # assert "100" in self.page.get_by_test_id("monthly-balance").inner_text()
-        # assert cashflow_item_count() == 1
-
         expect(self.page.locator('input[name="name"]')).to_have_value("")
-        expect(self.page.get_by_test_id("monthly-balance")).to_contain_text("100")
         expect(self.page.get_by_test_id(self.income_list_item_id)).to_have_count(1)
+        expect(self.page.get_by_test_id("income-list-item")).to_contain_text(
+            income_item["name"]
+        )
+        expect(self.page.get_by_test_id("income-list-item")).to_contain_text(
+            income_item["amount"]
+        )
 
         self.page.locator('select[name="type"]').select_option(label="Expense")
         self.page.locator('select[name="source"]').select_option(label="Regular Bill")
-        self.page.locator('input[name="name"]').fill("Phone")
-        self.page.locator('input[name="amount"]').fill("50")
+        self.page.locator('input[name="name"]').fill(expense_item["name"])
+        self.page.locator('input[name="amount"]').fill(expense_item["amount"])
         self.page.get_by_role("button", name="add").click()
 
         expect(self.page.locator('input[name="name"]')).to_have_value("")
-        expect(self.page.get_by_test_id("monthly-balance")).to_contain_text("50")
         expect(self.page.get_by_test_id("expense-list-item")).to_have_count(1)
+        expect(self.page.get_by_test_id("expense-list-item")).to_contain_text(
+            expense_item["name"]
+        )
+        expect(self.page.get_by_test_id("expense-list-item")).to_contain_text(
+            expense_item["amount"]
+        )
 
     def test_delete(self):
         """
@@ -54,7 +70,7 @@ class TestCashflows(BasePlaywrightTestCase):
         Then I should not see any cashflows
         """
 
-        self.cashflow = baker.make(Cashflow, type="income")
+        baker.make(_model=Cashflow, type="income")
         self.page.goto(f"{self.live_server_url}/")
 
         cashflow_item = self.page.get_by_test_id(self.income_list_item_id)
