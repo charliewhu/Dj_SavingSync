ef31af08b8a56b528f6b08b654f808dbb67a7260
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-07-19 10:58:08 +0100

    cached properties

--- src/cashflows/querysets.py
+++ src/cashflows/querysets.py
@@ -1,26 +1,29 @@
 from django.db import models
+from django.utils.functional import cached_property
+
 from django.db.models import Sum
 
 
 class CashflowQuerySet(models.QuerySet):
+    @cached_property
     def incomes(self):
         return self.filter(type="income")
 
+    @cached_property
     def expenses(self):
         return self.filter(type="expense")
 
+    @cached_property
     def monthly_balance(self):
-        incomes = (
-            self.filter(type="income").aggregate(amount=Sum("amount")).get("amount")
-        ) or 0
-        expenses = (
-            self.filter(type="expense").aggregate(amount=Sum("amount")).get("amount")
-        ) or 0
+        incomes = self.incomes.aggregate(amount=Sum("amount")).get("amount") or 0
+        expenses = self.expenses.aggregate(amount=Sum("amount")).get("amount") or 0
 
         return incomes - expenses
 
+    @cached_property
     def biannual_balance(self):
-        return self.monthly_balance() * 6
+        return self.monthly_balance * 6
 
+    @cached_property
     def annual_balance(self):
-        return self.monthly_balance() * 12
+        return self.monthly_balance * 12
